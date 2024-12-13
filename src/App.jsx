aa8b53acffdbf466a8e120428f40af69b1113e07
ShubhamThakur025 <thakur0805@gmail.com>
2024-12-13 10:34:38 +0530

    feat: dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,23 @@ import profileImage from "/assets/profile.jpg"
 import Introduction from "./sections/Introduction"
 import Skills from "./sections/Skills"
 import Contacts from "./sections/Contacts"
+import DarkModeToggle from "./components/DarkModeToggle"
 
 function App() {
   return (
-    <div className="bg-slate-100 flex flex-col justify-center items-center py-8">
+    <div className="bg-slate-100 dark:bg-slate-800 flex flex-col justify-center items-center py-8">
+      <DarkModeToggle />
       <img src={profileImage} className="w-40 rounded-full absolute top-10 " alt="profile-image" />
-      <div className="mt-28 w-[50vw] p-8 bg-white shadow rounded-xl pt-20">
+      <div className="mt-28 w-[50vw] p-8 dark:bg-gray-900 bg-white shadow rounded-xl pt-20">
         <Introduction />
-        <hr className="my-5" />
+        <hr className="my-5 border-slate-300 dark:border-slate-600" />
         <Skills />
-        <hr className="my-5" />
+        <hr className="my-5 border-slate-300 dark:border-slate-600" />
         <Projects />
-        <hr className="my-5" />
+        <hr className="my-5 border-slate-300 dark:border-slate-600" />
         <Contacts />
-        <hr className="my-5" />
-        <p className="text-center text-sm text-slate-500">Shubham Thakur - <span className="text-blue-500">2024</span></p>
+        <hr className="my-5 border-slate-300 dark:border-slate-600" />
+        <p className="text-center text-sm text-slate-500 dark:text-slate-400">Shubham Thakur - <span className="text-blue-500 dark:text-blue-400">2024</span></p>
       </div>
     </div>
   )
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,12 +4,12 @@ import { IoCodeSlash } from "react-icons/io5";
 
 export default function ProjectCard({ imageSrc, projectName, projectDescription, techStack, liveLink, repoLink }) {
     return (
-        <div className="bg-white mb-8 mx-auto border border-slate-300 border-2 rounded-lg shadow-lg p-5">
-            <div className="border border-slate-300 border-2 rounded-lg shadow p-3 w-[90%] mx-auto">
+        <div className="bg-white dark:bg-black mb-8 mx-auto border border-slate-300 dark:border-slate-900 border-2 rounded-lg shadow-lg p-5">
+            <div className="border border-slate-300 dark:border-slate-900 border-2 rounded-lg shadow dark:shadow-xl p-3 w-[90%] mx-auto">
                 <img src={imageSrc} alt={projectName} className="rounded mx-auto" />
             </div>
-            <h3 className="font-bold text-slate-800">{projectName}</h3>
-            <p className="text-slate-600 text-sm text-justify">{projectDescription}</p>
+            <h3 className="font-bold text-slate-800 dark:text-slate-200">{projectName}</h3>
+            <p className="text-slate-600 dark:text-slate-300 text-sm text-justify">{projectDescription}</p>
             <div className="flex justify-start items-center my-3">{techStack.map(skill => <div className={`text-center py-1 px-2 m-1 rounded text-sm pr-3 ${skills[skill].bgColor} border ${skills[skill].borderColor} ${skills[skill].textColor}`}>{skills[skill].skillName}</div>)} </div>
             <div className='flex'>
                 <a href={repoLink} target='_blank'><button className="text-sm px-3 py-2 text-white rounded bg-slate-800 flex items-center mr-3">View Project <IoCodeSlash size={18} className='ml-1.5' /></button></a>
